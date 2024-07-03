9f3b83873f8fc8e78dea8ba574a9f4d908e1f7f3
Ezra Kimutai <kimtaiezra02@gmail.com>
2024-07-03 10:40:15 +0300

    Recent Changes

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,57 +1,56 @@
-import { useEffect, useState } from 'react';
+import {useState, useEffect} from "react"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 
+export const NavBar=()=> {
+    const [activeLink, setActiveLink]= useState('home');
+    const [scrolled,setScrolled]=useState(false);
 
-export const NavBar = () => {
-  const [activeLink, setActiveLink] = useState('home');
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
+    useEffect(()=>{
+        const onScroll =() =>{
+        if(window.scrollY>50){
+            setScrolled(true);
+        }else{
+            setScrolled(false);
+        }
     }
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [])
-  const onUpdateActiveLink = (value: string) => {
-    setActiveLink(value);
-  }
+
+        window.addEventListener("scroll", onscroll);
+        return ()=>window.removeEventListener("scroll",onScroll);
+    },[])
+
+    const onUpdateActiveLink= (value: any) => {
+        setActiveLink(value);
+    }
   return (
-    <>
-      <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
-        <Container>
-          <Navbar.Brand href="#home"><img src={"url(./src/assets/img/logo.svg)"} alt="LOGO" /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" >
-            <span className="Navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-
-            </Nav>
-            <span className="navbar-text">
-              <div className='social-icon'>
-              <img src={"url(./src/assets/img/nav-icon1.svg)"} alt="ICON" />
-              <img src={"url(./src/assets/img/nav-icon2.svg)"} alt="ICON" />
-              <img src={"url(./src/assets/img/nav-icon3.svg)"} alt="ICON" />
-              </div>
-              <button className="vvd"><span>Lets Connect</span></button>
-
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""} >
+      <Container>
+        <Navbar.Brand href="#home"><img src={logo} alt="Logo"/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" >
+            <span className='navbar-toggler-icon'></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
+          <span className='navBar-text'>
+            <div className='social-icon'>
+                <a ><img src={navIcon1} alt=''/></a>
+                <a ><img src={navIcon2} alt=''/></a>
+                <a ><img src={navIcon3} alt=''/></a>
+            </div>
+            <button className='vvd' onClick={()=>console.log('connect')}><span>Lets Connect</span></button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
-export default NavBar
-
--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -1,59 +1,66 @@
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
 
-export const Skills = () => {
+
+export const Skills =()=>{
     const responsive = {
         superLargeDesktop: {
-
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
         },
         desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
         },
         tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
         },
         mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
+          breakpoint: { max: 464, min: 0 },
+          items: 1
         }
-    };
+      };
+return(
+    <section className="skill" id="skills">
+        <Container>
+            <Row>
+                <Col>
+                <div className='skil-bx'>
+                  <h2>
+                    Skills
+                  </h2>
+                  <p> My goal is to leverage my expertise to solve real-world problems through technology, contributing to projects that make a meaningful impact. When I'm not coding, you can find me exploring new tech ideas, collaborating with other developers, or diving into the latest design trends. Let's connect and create something amazing together!</p>
+                  <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                    <div className="item">
+                      <img src={meter1} alt="Image1"/> 
+                      <h5>Web Development</h5>
+                    </div>
+                    <div className="item">
+                      <img src={meter2} alt="Image2"/> 
+                      <h5>Product Design</h5>
+                    </div>
+                    <div className="item">
+                      <img src={meter3} alt="Image3"/> 
+                      <h5>Fintech Engineering</h5>
+                    </div>
+                    <div className="item">
+                      <img src={meter1} alt="Image4"/> 
+                      <h5>Brand Identity</h5>
+                    </div>
+                  </Carousel>
+                </div>
+                </Col>
+            </Row>
+        </Container>
+        <img className="background-image-left" src={colorSharp} alt='ColorS'/>
 
-    return (
-        <section className="skill" id="skills">
-            <Container>
-                <Row>
-                    <col>
-                        <div className="skill-bx">
-                            <h2>
-                                Skills
-                            </h2>
-                            <p>Strong knowledge of modern JavaScript and TypeScript. Skilled in building dynamic web applications using frameworks like React</p>
-                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                                <div className="item">
-                                <img src={"url(./src/assets/img/meter1.svg)"} alt="IMG" />
-                                    <h5>Web Development</h5>
-                                </div>
-                                <div>
-                                <img src={"url(./src/assets/img/meter2.svg)"} alt="IMG" />
-                                    <h5>UI/UX Design</h5>
-                                </div>
-                                <div>
-                                <img src={"url(./src/assets/img/meter2.svg)"} alt="IMG" />
-                                    <h5>Graphic Design</h5>
-                                </div>
-                            </Carousel>
-                        </div>
-                    </col>
-                </Row>
-            </Container>
-            <img src={"url(./src/assets/img/color-sharp.png)"} alt="Header Img" />
-        </section>
-    )
+    </section>
+)
 }
-
-export default Skills
