97a56ea314de4c861d8947460db1bebeda810c4d
Ezra Kimutai <kimtaiezra02@gmail.com>
2024-07-04 10:54:08 +0300

    Updates

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -2,14 +2,16 @@ import { useState, useEffect } from "react";
 import { Row,Col, Container } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
+import TrackVisibility from 'react-on-screen';
+import 'animate.css';
 
 export const Banner= () =>{
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting]= useState(false);
-    const toRotate = ["Web Developer", "UI/UX Designer", "Fintech Engineer"];
+    const toRotate = ["Web Developer", "UI/UX Designer", "Software Architecture"];
     const [text, setText]= useState('');
-    const [delta, setDelta]= useState(300-Math.random()*100);
-    const period= 2000;
+    const [delta, setDelta]= useState(150-Math.random()*50);
+    const period= 1000;
 
     useEffect(()=>{
         let ticker= setInterval(()=>{
@@ -17,7 +19,7 @@ export const Banner= () =>{
         },delta)
 
         return ()=>{clearInterval(ticker)};
-    },[text])
+    },[text, delta])
 
 const tick =()=>{
     let i =loopNum % toRotate.length;
@@ -35,20 +37,24 @@ const tick =()=>{
     }else if(isDeleting && updatedText === ''){
         setIsDeleting(false);
         setLoopNum(loopNum + 1);
-        setDelta(500);
+        setDelta(150 - Math.random() * 50);
 
     }
 }
-
     return(
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
+                    <TrackVisibility>
+                    {({ isVisible }) =>
+                    <div className= {isVisible ? "animate__flipInY" : ""}>
                         <span className="tagline">Welcome to my portfolio</span>
                         <h1>{`Hi i'm Ezra Kimutai `}<span className="wrap">{text}</span></h1>
                         <p>I am a passionate Software Developer who aims at solving real world problems using technology. Advancing my skills and learning new ones is what I enjoy doing the most. Dive deeper into my projects and skills to see what I can bring to the table!</p>
                         <button onClick={()=> console.log('connect')}>Lets connect <ArrowRightCircle size={25} /> </button>
+                    </div>}
+                    </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header Img"/>                  
--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,21 +1,26 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
 export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
+          <MailchimpForm/>
           <Col size={12} sm={6}>
-          <img src={"url(../src/assets/img/logo.svg)"} alt="Header Img" />
+            <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-            <img src={"url(../src/assets/img/nav-icon1.svg)"} alt="Header Img" />
-            <img src={"url(../src/assets/img/nav-icon2.svg)"} alt="Header Img" />
-            <img src={"url(../src/assets/img/nav-icon3.svg)"} alt="Header Img" />
+              <a href="https://www.linkedin.com/in/ezra-kimutai-20a287224"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.facebook.com/profile.php?id=100083594628866&mibextid=ZbWKwL"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/its_kimtai/?hl=en"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2024. Ezra Kimutai</p>
+            <p>Copyright 2024. Ezra Kimutai.</p>
           </Col>
         </Row>
       </Container>
